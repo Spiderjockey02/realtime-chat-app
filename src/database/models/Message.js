eee1391b291dabc0ea88c18111665f0b35296b36
Spiderjockey02 <benjamin.forey11@gmail.com>
2022-07-17 22:27:23 +0100

    A very basic file uploader

--- src/database/models/Message.js
+++ src/database/models/Message.js
@@ -3,6 +3,8 @@ const { Schema, model } = require('mongoose');
 const messageSchema = Schema({
 	name: String,
 	message: String,
+	type: { default: 'message' },
+	imageURL: String
 });
 
 module.exports = model('Message', messageSchema);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express'),
 	app = express(),
 	http = require('http').Server(app),
 	{ port } = require('./config'),
+	fs = require('fs'),
+	siofu = require("socketio-file-upload"),
 	io = require('socket.io')(http);
 
 // connect to database
@@ -13,14 +15,18 @@ app.use(express.static(__dirname))
 	.use(bodyParser.json())
 	.use(bodyParser.urlencoded({ extended: false }))
 	.engine('html', require('ejs').renderFile)
+	.use(siofu.router)
 	.set('view engine', 'ejs')
 	.set('views', './src/views')
 	.use('/', require('./routes'))
 	.use('/api', require('./routes/api')(io));
 
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
 	console.log('a user is connected');
+	var uploader = new siofu();
+  uploader.dir = __dirname + '/uploads';
+  uploader.listen(socket);
 });
 
 http.listen(port, () => {
