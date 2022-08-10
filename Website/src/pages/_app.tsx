0f43cdbbc006ab257154a1c2ddbb81584e9d1c25
GoldenCredit <birongliu2003@gmail.com>
2022-08-10 17:56:51 -0400

    using require to load bootstrap instead import which void the error

--- Website/src/pages/_app.tsx
+++ Website/src/pages/_app.tsx
@@ -2,8 +2,10 @@ import { SessionProvider } from 'next-auth/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { SocketProvider } from '../components/socketio';
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+	useEffect(() => require('bootstrap/dist/js/bootstrap'));
 	return (
 		<SessionProvider session={session}>
 			<SocketProvider>
