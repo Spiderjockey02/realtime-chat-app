112f86da57480e7c2706f77f1b561c5635b12f68
GoldenCredit <birongliu2003@gmail.com>
2022-08-18 10:50:57 -0400

    [api] add validateRoute and fix bug

--- api/src/utils/Utils.ts
+++ api/src/utils/Utils.ts
@@ -2,17 +2,24 @@ import { readdirSync, statSync } from 'fs';
 import { join, parse, sep } from 'path';
 
 export class Utils {
-	public static generateRoutes(directory: string, prefix: string, seperator = '/') {
+	public static generateRoutes(directory: string, prefix: string) {
 		const results: FileOptions[] = [];
 		for(const path of this.searchDirectory(directory)) {
-			const { dir, name } = parse(file);
+			const { dir, name } = parse(path);
 			const basePath = directory.split(sep).pop() as string;
-			const dirIndex = dir.indexOf(basePath);
-			const routePath = dir.slice(dirIndex).split(sep).join(seperator).toString().replace(basePath, prefix.startsWith(seperator) ? prefix : `${seperator}${prefix}`);
-			if(routePath.includes('[') && routePath.includes(']') || name.includes('[') && name.includes(']')) results.push({ path, route: `${routePath.replace('[', ':').replace(']', '')}${seperator}${name.replace('[', ':').replace(']', '')}` });
-			else results.push({ path, route: `${routePath}${seperator}${name}` });
+			const directoryRoute = dir.substring(dir.indexOf(basePath)).replace(basePath, prefix.startsWith('/') ? prefix : `/${prefix}`);
+			const { result: dynamicDirRoute } = this.validateDynamicRoute(directoryRoute);
+			const { result: dynamicFileRoute } = this.validateDynamicRoute(name, true);
+			results.push({ path, route: `${dynamicDirRoute}${dynamicFileRoute}` });
+
 		}
 		return results;
+	}
+	private static validateDynamicRoute(context: string, isFile = false, seperator = '/') {
+		const dynamicRouteValidator = /(?<=\[).+?(?=\])/gi;
+		const validate = (dynamicRouteValidator.exec(context));
+		if(!validate) return { result: isFile ? `${seperator}${context}` : context };
+		return { result: context.replace(`[${validate[0]}]`, isFile ? `${seperator}:${validate[0]}` : `:${validate[0]}`) };
 	}
 
 	public static searchDirectory(directory: string, files: string[] = []) {
